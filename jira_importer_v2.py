df715f2fc3d74362a4aec2b7a4f1cb1ec25c6288
Isabelle Sharman <isharman@yext.com>
2025-09-10 14:14:45 -0400

    Update jira_importer_v2.py
    Fix bracket placement

--- jira_importer_v2.py
+++ jira_importer_v2.py
@@ -97,9 +97,9 @@ def export_jira_data(jira_url: str, jira_email: str, jira_api_token: str, jql_qu
 
     params = {
     "jql": jql_query,
-    "maxResults": 3000,
+    "maxResults": 1000,
     "fields": "summary,description,reporter,assignee,created,status,customfield_17591,customfield_17636,customfield_14707"
-}
+    }
 
     try:
         response = requests.get(api_url, headers=headers, auth=(jira_email, jira_api_token), params=params)
